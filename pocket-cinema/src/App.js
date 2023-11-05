9a617595ab214d893ec75e646899b18034a537ce
Sarah McGowan <sarahmcgowan@Sarahs-MacBook-Pro-3.local>
2023-11-05 14:49:53 -0600

    task 3

--- pocket-cinema/src/App.js
+++ pocket-cinema/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // Add state using useState hook
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    // Get the value of accessToken from localStorage
+    const accessToken = localStorage.getItem('accessToken');
+
+    // Check if there's an accessToken
+    if (accessToken) {
+      // Send a post request to /api/auth/ with the authorization header
+      fetch('/api/auth/', {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      })
+        .then((response) => {
+          // Check if the request was successful
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Authentication failed');
+          }
+        })
+        .then((data) => {
+          // Set the isLoggedIn state to true and userUsername from the response
+          setIsLoggedIn(true);
+          setUserUsername(data.username);
+        })
+        .catch((error) => {
+          console.error('Authentication error:', error);
+        });
+    }
+  }, []);
+
+  // Render different components based on the isLoggedIn state
+  return isLoggedIn ? <Dashboard username={userUsername} /> : <Authentication />;
 }
 
 export default App;

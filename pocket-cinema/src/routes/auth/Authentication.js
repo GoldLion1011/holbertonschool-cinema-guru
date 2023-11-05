982d56e12aedb8e8a62d9426cf7b21332d9f3f4b
Sarah McGowan <sarahmcgowan@Sarahs-MacBook-Pro-3.local>
2023-11-05 15:57:49 -0600

    task 5, do css next

--- pocket-cinema/src/routes/auth/Authentication.js
+++ pocket-cinema/src/routes/auth/Authentication.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import './auth.css';
+import axios from 'axios';
 import Login from './Login';
 import Register from './Register';
 
 
 const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
   const [switchState, setSwitchState] = useState(true);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignInClick = () => {
     setSwitchState(true);
@@ -13,6 +16,43 @@ const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
 
   const handleSignUpClick = () => {
     setSwitchState(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (switchState) {
+        // Logging in
+        const response = await axios.post('/api/auth/login', {
+          username,
+          password,
+        });
+
+        // Store the token in localStorage
+        localStorage.setItem('accessToken', response.data.accessToken);
+
+        // Set userUsername and isLoggedIn state
+        setUserUsername(username);
+        setIsLoggedIn(true);
+      } else {
+        // Registering
+        const response = await axios.post('/api/auth/register', {
+          username,
+          password,
+        });
+
+        // Store the token in localStorage
+        localStorage.setItem('accessToken', response.data.accessToken);
+
+        // Set userUsername and isLoggedIn state
+        setUserUsername(username);
+        setIsLoggedIn(true);
+      }
+    } catch (error) {
+      console.error('Authentication error:', error);
+      // Handle any error or validation logic here
+    }
   };
 
   return (
@@ -31,7 +71,10 @@ const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
           Sign Up
         </button>
       </div>
-      {switchState ? <Login /> : <Register />}
+      {switchState ? <Login username={username} password={password} setUsername={setUsername} setPassword={setPassword} /> : <Register username={username} password={password} setUsername={setUsername} setPassword={setPassword} />}
+      <button onClick={handleSubmit} type="submit">
+        {switchState ? "Sign In" : "Register"}
+      </button>
     </div>
   );
 };
